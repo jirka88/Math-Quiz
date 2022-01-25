d4b7315077633f86b34d6ec1003e69c817dbe951
ondrejsvorc <ondrejsvorc@email.cz>
2022-01-25 20:24:07 +0100

    code clean-up

--- script.js
+++ script.js
@@ -1,54 +1,58 @@
 const problem = document.querySelector('.problem')
-var optionDivs = document.querySelectorAll('.option')
+const optionDivs = document.querySelectorAll('.option')
 const options = document.querySelectorAll('.option-value')
 const correct = document.querySelector(".correct");
 const incorrect = document.querySelector(".incorrect");
 
-class GameService { 
-    CorrectCount = 0;
-    IncorrectCount = 0;
+class GameService {
+
+    correctCount = 0;
+    incorrectCount = 0;
+
     newProblem() {
         let signs = ["+", "-"]
         let sign = signs[Math.floor(Math.random() * 2)]
         let number1 = Math.floor(Math.random() * 100) + 1
         let number2 = Math.floor(Math.random() * 100) + 1
-        if (number1 < number2 && sign == "-") 
+        if (number1 < number2 && sign == "-")
             sign = signs[0]
-            
+
         problem.innerHTML = `${number1} ${sign} ${number2} = ?`
-        
-        if(sign === "+")
+
+        if (sign === "+")
             this.result = number1 + number2
         else
             this.result = number1 - number2
 
         this.getOptions()
     }
+
     getOptions() {
-        let randomRight = Math.floor(Math.random() * 3) // 0 - 2 (3 možnosti)
-        let count = 0 
+        let randomRight = Math.floor(Math.random() * 3)  // 0 - 2 (3 možnosti)
+        let count = 0
         options.forEach(option => {
-            if(count === randomRight) {
+            if (count === randomRight) {
                 option.innerHTML = this.result
-                optionDivs[count].setAttribute("value","true") //nastavení value
-            }             
+                optionDivs[count].setAttribute("value", "true") //nastavení value
+            }
             else {
                 option.innerHTML = Math.floor(Math.random() * 100) + 1
-                optionDivs[count].setAttribute("value","false") 
-            }     
+                optionDivs[count].setAttribute("value", "false")
+            }
             count++
         })
     }
+
     compareResultWithOption(option) {
-        if(option == "true") {      //vyhodnocuje pomocí value v tlačítku 
-            this.CorrectCount++;
-            correct.innerHTML = "Correct answers: " + this.CorrectCount;
+        if (option == "true") {      // Vyhodnocuje pomocí value v tlačítku 
+            this.correctCount++;
+            correct.innerHTML = "Correct: " + this.correctCount;
         }
         else {
-            this.IncorrectCount++;
-            incorrect.innerHTML = "Incorrect answers: " + this.IncorrectCount;
+            this.incorrectCount++;
+            incorrect.innerHTML = "Incorrect: " + this.incorrectCount;
         }
-        this.newProblem() //vytvoří se nový příklad i při špatném výpočtu = zaznamená se v proměnné "IncorrectCount"
+        this.newProblem() // Vytvoří se nový příklad i při špatném výpočtu = zaznamená se v proměnné "incorrectCount"
     }
 }
 
@@ -60,5 +64,5 @@ window.onload = function () {
             gameService.compareResultWithOption(option.getAttribute("value")) //získá value 
         })
     })
-   gameService.newProblem()
+    gameService.newProblem()
 }
