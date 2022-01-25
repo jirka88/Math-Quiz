79254cef8d198ccb14d7db0a3fc65fcd010970f9
Ondřej Švorc <ondrejsvorc@email.cz>
2022-01-25 08:05:45 +0100

    OOP changes

--- script.js
+++ script.js
@@ -1,83 +1,49 @@
 const problem = document.querySelector('.problem')
+const optionDivs = document.querySelectorAll('.option')
 const options = document.querySelectorAll('.option-value')
-const options1 = document.querySelectorAll('.option')
 
-var vysledek;
-
-newProblem = () => {
-    let znamenka = ["+","-"];
-    let znamenko = znamenka[Math.floor(Math.random() * 2)];
-    let number1 = Math.floor(Math.random() * 101);
-    let number2 = Math.floor(Math.random() * 101);
-    problem.innerHTML = `${number1} ${znamenko} ${number2} = ?`
-
-    if(znamenko === "+") {
-        vysledek = number1 + number2;
-    }
-    else {
-        vysledek = number1 - number2;
-    }     
-    getOptions();
-}
-getOptions = () => {
-    let randomRight = Math.floor(Math.random() * 3) // 0 - 2 (3 možnosti)
-    count = 0; 
-    options.forEach(option => {
-        if(count === randomRight) {
-                option.innerHTML = vysledek;
-        }
-        else {
-            option.innerHTML = Math.floor(Math.random() * 101)
-        }
-        count++;
-    });
-}
-compareResultWithOption = (option) => {
-    if(vysledek == option.innerHTML) {
-        newProblem();
-    }
-}
-
-/*class GameService {
+class GameService { 
     newProblem() {
-        problem.innerHTML = `${Math.floor(Math.random() * 101)} + ${Math.floor(Math.random() * 101)} = ?`
+        let signs = ["+","-"]
+        let sign = signs[Math.floor(Math.random() * 2)]
+        let number1 = Math.floor(Math.random() * 101)
+        let number2 = Math.floor(Math.random() * 101)
+        problem.innerHTML = `${number1} ${sign} ${number2} = ?`
+
+        if(sign === "+")
+            this.result = number1 + number2
+        else
+            this.result = number1 - number2
+
         this.getOptions()
     }
     getOptions() {
         let randomRight = Math.floor(Math.random() * 3) // 0 - 2 (3 možnosti)
-        let count = 0
-        let result = this.getResult()
-        for (const option of options) {
-            if (randomRight == count) {
-                option.innerHTML = result
-                count = -1
-            }
-            else {
+        let count = 0 
+        options.forEach(option => {
+            if(count === randomRight)
+                option.innerHTML = this.result
+            else
                 option.innerHTML = Math.floor(Math.random() * 101)
-                count++
-            }
-        }
-    }
-    getResult() {
-        let test = problem.innerHTML.split(' ')
-        return Number(test[0]) + Number(test[2])
+
+            count++
+        })
     }
     compareResultWithOption(option) {
-        let result = this.getResult()
-        if (option.innerHTML == result) {
+        if(this.result == option.innerHTML) {
             this.newProblem()
         }
     }
 }
 
 const gameService = new GameService()
-*/
+
 window.onload = function () {
-    options1.forEach(option => {
+    optionDivs.forEach(option => {
         option.addEventListener('click', event => {
-            compareResultWithOption(event.target)
+            gameService.compareResultWithOption(event.target)
         })
     })
-   newProblem()
+   gameService.newProblem()
 }
 
