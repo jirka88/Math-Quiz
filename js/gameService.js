511f423d7cd153a4ae89876b4342ec6a11a8d773
ondrejsvorc <52735242+ondrejsvorc@users.noreply.github.com>
2022-01-30 12:26:26 +0100

    code clean-up, architecture update

---
+++ js/gameService.js
@@ -0,0 +1,58 @@
+export class GameService {
+
+    problem = document.querySelector('.problem')
+    optionDivs = document.querySelectorAll('.option')
+    options = document.querySelectorAll('.option-value')
+    correct = document.querySelector(".correct");
+    incorrect = document.querySelector(".incorrect");
+
+    correctCount = 0;
+    incorrectCount = 0;
+
+    newProblem(problem) {
+        let signs = ["+", "-"]
+        let sign = signs[Math.floor(Math.random() * 2)]
+        let number1 = Math.floor(Math.random() * 100) + 1
+        let number2 = Math.floor(Math.random() * 100) + 1
+        if (number1 < number2 && sign == "-")
+            sign = signs[0]
+
+        this.problem.innerHTML = `${number1} ${sign} ${number2} = ?`
+
+        if (sign === "+")
+            this.result = number1 + number2
+        else
+            this.result = number1 - number2
+
+        this.getOptions()
+    }
+
+    getOptions() {
+        let randomRight = Math.floor(Math.random() * 3)  // 0 - 2 (3 možnosti)
+        let count = 0
+        this.options.forEach(option => {
+            if (count === randomRight) {
+                option.innerHTML = this.result
+                this.optionDivs[count].setAttribute("value", "Correct")
+            }
+            else {
+                option.innerHTML = Math.floor(Math.random() * 100) + 1
+                this.optionDivs[count].setAttribute("value", "Incorrect")
+            }
+            count++
+        })
+    }
+
+    compareResultWithOption(optionValue) {
+        if (optionValue == "Correct") {
+            this.correctCount++
+            this.correct.innerHTML = `${optionValue}: ${this.correctCount}`
+        }
+        else {
+            this.incorrectCount++;
+            this.incorrect.innerHTML = `${optionValue}: ${this.incorrectCount}`
+        }
+
+        this.newProblem() // Vytvoří se nový příklad i při špatném výpočtu = zaznamená se v proměnné "incorrectCount"
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,12 @@
+import { GameService } from "./gameService.js";
+
+const gameService = new GameService()
+
+window.onload = function () {
+    gameService.optionDivs.forEach(option => {
+        option.addEventListener('click', event => {
+            gameService.compareResultWithOption(option.getAttribute("value"))
+        })
+    })
+    gameService.newProblem()
+}
